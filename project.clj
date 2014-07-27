d0b94247c8cc2447353d26ddb55ff7dcde13af5e
jiro <j.kugiya@gmail.com>
2014-07-27 12:29:19 +0900

    Revert project.clj to pull

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
-(defproject ; original cljs-kickoff/lein-template "0.1.5"
-  jkugiya/lein-template "0.1.6-SNAPSHOT"
+(defproject cljs-kickoff/lein-template "0.1.6"
   :description "Leiningen template for ClojureScript projects with lein-cljsbuild"
-  ; original :url "https://github.com/konrad-garus/cljs-kickoff"
-  :url "https://github.com/jkugiya/cljs-kickoff"
+  :url "https://github.com/konrad-garus/cljs-kickoff"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
